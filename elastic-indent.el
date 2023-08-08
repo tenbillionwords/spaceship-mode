028f365779994f09e4fe22b4c98b4529aaa00afb
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2023-08-08 10:52:48 +0200

    disable messages

--- elastic-indent.el
+++ elastic-indent.el
@@ -227,13 +227,13 @@ this way."
       (save-excursion
         (when (eq (forward-line -1) 0)
           (setq reference-pos (progn (move-to-column start-col) (point)))))
-      (message "%s: first line. update from startcol=%s curcol=%s" (line-number-at-pos) start-col (current-column))
+      ;; (message "%s: first line. update from startcol=%s curcol=%s" (line-number-at-pos) start-col (current-column))
       (when (elastic-indent-in-indent) ; if not characters are not to be changed.
         (char-loop))
       (next-line)
       (when (or force-propagate (<= (point) change-end))
         ;; this is a multiline change. It influences column 0.
-        (message "%s: main phase; update lines from col 0" (line-number-at-pos))
+        ;; (message "%s: main phase; update lines from col 0" (line-number-at-pos))
         (when (> start-col 0)  ; reference is wrong now
           (setq start-col 0)
           (setq prev-widths nil)
@@ -241,11 +241,11 @@ this way."
         (while (<= (point) change-end)
           (char-loop)
           (next-line)))
-      (message "%s: propagate changes and stop if indentation is too small" (line-number-at-pos))
+      ;; (message "%s: propagate changes and stop if indentation is too small" (line-number-at-pos))
       (while (not (elastic-indent-column-leaves-indent start-col))
         (char-loop)
         (next-line))
-      (message "%s: propagation complete" (line-number-at-pos))
+      ;; (message "%s: propagation complete" (line-number-at-pos))
       (beginning-of-line)))) ; we did not in fact propagate on this line yet.
 
 (defun elastic-indent-change-extend (end)
@@ -265,7 +265,7 @@ buffer.  Propagation means to also fix the indentation of the
 lines which follow, if their indentation widths might be impacted
 by changes in given region.  See `elastic-indent-do' for the
 explanation of FORCE-PROPAGATE."
-  (message "edr: (%s) %s-%s" force-propagate start end)
+  ;; (message "edr: (%s) %s-%s" force-propagate start end)
   (let ((e (elastic-indent-change-extend end)))
     (elastic-indent-clear-region start e)
     (goto-char start)
